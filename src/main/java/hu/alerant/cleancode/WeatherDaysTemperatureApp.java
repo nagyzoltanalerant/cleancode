beb93e3baf24cc7e3c47c7a1c492fbc46a58eb76
nagyzoltan <nagy.zoltan@alerant.hu>
2022-02-14 10:58:30 +0100

    cleanup

--- src/main/java/hu/alerant/cleancode/WeatherDaysTemperatureApp.java
+++ src/main/java/hu/alerant/cleancode/WeatherDaysTemperatureApp.java
@@ -9,36 +9,38 @@ import java.util.Arrays;
 import java.util.List;
 
 public class WeatherDaysTemperatureApp {
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    WeatherDaysTemperatureApp app = new WeatherDaysTemperatureApp();
+        WeatherDaysTemperatureApp app = new WeatherDaysTemperatureApp();
 
-    List<String> allLines = null;
-    try {
-      allLines = Files.readAllLines(Paths.get(ClassLoader.getSystemResource("datamunging/weather.dat").toURI()));
-      BigDecimal foundMinimum = BigDecimal.valueOf(Long.MAX_VALUE);
-      BigDecimal foundMinimumRow = BigDecimal.ZERO;
-      for (String line: allLines.subList(2,allLines.size()-1)) {
-        List<String> lineColumns = Arrays.asList(line.split("\\s+"));
+        String fileName = "datamunging/weather.dat";
+        BigDecimal foundMinimum = BigDecimal.valueOf(Long.MAX_VALUE);
+        BigDecimal foundMinimumRow = BigDecimal.ZERO;
 
-        BigDecimal max = BigDecimal.valueOf(Long.parseLong(lineColumns.get(2).replaceAll("\\*","")));
-        BigDecimal min = BigDecimal.valueOf(Long.parseLong(lineColumns.get(3).replaceAll("\\*","")));
-        BigDecimal actualDiff = max.subtract(min);
-        if (actualDiff.compareTo(foundMinimum) < 0) {
-          foundMinimum = actualDiff;
-          foundMinimumRow = BigDecimal.valueOf(Long.parseLong(lineColumns.get(1)));
+        try {
+            List<String> allLines = allLines = Files.readAllLines(Paths.get(ClassLoader.getSystemResource(fileName).toURI()));
+
+            for (String line : allLines.subList(2, allLines.size() - 1)) {
+                List<String> lineColumns = Arrays.asList(line.split("\\s+"));
+
+                BigDecimal max = BigDecimal.valueOf(Long.parseLong(lineColumns.get(2).replaceAll("\\*", "")));
+                BigDecimal min = BigDecimal.valueOf(Long.parseLong(lineColumns.get(3).replaceAll("\\*", "")));
+                BigDecimal actualDiff = max.subtract(min);
+
+                if (actualDiff.compareTo(foundMinimum) < 0) {
+                    foundMinimum = actualDiff;
+                    foundMinimumRow = BigDecimal.valueOf(Long.parseLong(lineColumns.get(1)));
+                }
+            }
+            System.out.println("Found minimum diff:" + foundMinimum);
+            System.out.println("Found minimum diff, row number:" + foundMinimumRow);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
         }
-      }
-      System.out.println("Found minimum diff:"+foundMinimum);
-      System.out.println("Found minimum diff, row number:"+foundMinimumRow);
-      
-    } catch (IOException e) {
-      e.printStackTrace();
-    } catch (URISyntaxException e) {
-      e.printStackTrace();
+
     }
-
-
-  }
 
 }
